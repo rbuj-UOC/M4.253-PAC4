7d8f9294173e20d61a57574baffe6cb9e8e72990
Robert Buj <rbuj@users.noreply.github.com>
2024-07-10 01:34:56 +0200

    Refactor eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  pluginReactConfig,
   {
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...pluginReactConfig.rules,
-      indent: ["error", 2],
-      "prefer-const": "error",
-      semi: "error"
+      indent: ['error', 2],
+      'prefer-const': 'error',
+      semi: 'error'
     }
-  },
-  pluginJs.configs.recommended,
-  pluginReactConfig
+  }
 ];
