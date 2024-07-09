bd3ac2ac1b83752cc1dc3a48a1463c8ebcd2b00a
Robert Buj <rbuj@users.noreply.github.com>
2024-07-10 01:01:56 +0200

    honor: npm start

--- src/web/index.js
+++
@@ -1,9 +0,0 @@
-import { addListeners } from "../pec4/pec4.js";
-
-export async function startApp() {
-  addListeners();
-}
-
-window.addEventListener("DOMContentLoaded", async () => {
-  await startApp();
-});
